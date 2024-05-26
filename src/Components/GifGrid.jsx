cf1baab1c0b372189d8bd0fd380f6e2aaa65d6ce
DidioArbey <didioarbey@gmail.com>
2024-05-26 18:01:11 -0500

    mostrar titulos de las categorias

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -1,10 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
     useEffect(()=>{
-        getGifs(category);
+        getImages();
     },[ ])
 
 
@@ -12,6 +19,14 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3> {category} </h3>
+
+            <ol>
+                {
+                    images.map (( {id, title} )=>(
+                        <li key={id}> {title} </li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
