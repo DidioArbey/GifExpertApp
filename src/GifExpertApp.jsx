8b73420032814ea58d4505e9c5da74db93ff0889
DidioArbey <didioarbey@gmail.com>
2024-05-26 18:34:33 -0500

    mostrar imagenes y agregar classname css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { GifGrid } from "./Components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['one Puch']);
+    const [categories, setCategories] = useState(['one Punch']);
 
     const onAddCategory = (onNewCategory) => {
         if (categories.includes(onNewCategory)) return;
